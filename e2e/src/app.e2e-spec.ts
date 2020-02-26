a29249e4d61f673c100e33f2939b8b0bdda338b5
Umesh Chandra <UmeshC@hcl.com>
2020-02-26 15:03:46 +0530

    updated dashboard

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,23 +1,36 @@
-import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { AppPage } from "./app.po";
+import { browser, logging, element, by } from "protractor";
 
-describe('workspace-project App', () => {
+describe("workspace-project App", () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it("should display welcome message", () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angulartestcases app is running!');
+    expect(page.getTitleText()).toEqual("angulartestcases app is running!");
   });
 
+  it("should add one and two", function() {
+    browser.get("http://juliemr.github.io/protractor-demo/");
+    element(by.model("first")).sendKeys(1);
+    element(by.model("second")).sendKeys(2);
+
+    element(by.id("gobutton")).click();
+    expect(element(by.binding("latest")).getText()).toEqual("5"); // This is wrong!
+  });
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    const logs = await browser
+      .manage()
+      .logs()
+      .get(logging.Type.BROWSER);
+    expect(logs).not.toContain(
+      jasmine.objectContaining({
+        level: logging.Level.SEVERE
+      } as logging.Entry)
+    );
   });
 });
